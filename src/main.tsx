c673b7f27af65d3d673ea0e3c5983a41fd3d18c5
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-06-30 12:43:08 +0500

    Main Page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './app/global.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './app/layout';
+import Home from './app/page';
 
 const router = createBrowserRouter([{
   path: '/',
