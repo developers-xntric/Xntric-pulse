85f670c64e5037cd6d1d94333831fab29366c9e2
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-06-30 11:18:44 +0500

    Removed all css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './app/global.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './app/layout';
 
