beaa904443a11edf7e020583ccf83367437ae6b5
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-06-30 11:14:53 +0500

    Router Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout />,
+  children: [{
+    path: "",
+    element: <Home />,
+  },]
+}]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
+  <RouterProvider router={router}>
+  </RouterProvider>
 )
